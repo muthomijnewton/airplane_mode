5e42c8b17c3625c2ebc4c76e28616ec91d44b0b5
muthomijnewton <muthomijnewton@gmail.com>
2026-07-23 16:47:14 +0300

    Add Airplane Ticket Add-on Item, and Airplane Ticket Add-on Type Doctypes

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,34 @@
 # Copyright (c) 2026, Newton Muthomi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AirplaneTicket(Document):
-	pass
+	def validate(self):
+		self.remove_duplicate_add_ons()
+		self.calculate_total_amount()
+
+	def before_submit(self):
+		if self.status != 'Boarded':
+			frappe.throw("Only boarded tickets can be submitted")
+
+	def remove_duplicate_add_ons(self):
+		unique_items = []
+		seen = set()
+
+		for add_on in add_ons:
+			if add_on.item not in seen:
+				seen.add(add_on.item)
+				unique_items.append(add_on)
+
+		self.add_ons = unique_items
+
+	def calculate_total_amount(self):
+		total = self.flight_price
+
+		for add_on in self.add_ons:
+			total += add_on.amount or 0
+
+		self.total_amount = total
--- airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2026, Newton Muthomi and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FlightPassenger(Document):
-	pass
+	def before_save(self):
+		self.full_name = f"{self.first_name} {self.last_name}" if self.last_name else f"{self.first_name}"
