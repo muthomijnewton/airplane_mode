c05c6235312560172ac34c80e426a40dab8df17e
muthomijnewton <muthomijnewton@gmail.com>
2026-07-27 05:35:16 +0300

    Added a website link to the airline form. Added an 'Assign Seat' button to airplane ticket form.

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, Newton Muthomi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airline", {
+	refresh(frm) {
+        if (frm.doc.website) {
 
-// 	},
-// });
+            frm.add_web_link(
+                frm.doc.website,
+                "Visit Website"
+            );
+        }
+	},
+});
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2026, Newton Muthomi and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+	refresh(frm) {
 
-// 	},
-// });
+        frm.add_custom_button("Assign Seat", function() {
+
+            let dialog = new frappe.ui.Dialog({
+                title: "Assign Seat",
+                fields: [
+                    {
+                        label: "Seat Number",
+                        fieldname: "seat",
+                        fieldtype: "Data",
+                        reqd: 1
+                    }
+                ],
+
+                primary_action_label: "Assign",
+                primary_action(values) {
+                    frm.set_value('seat', values.seat);
+
+                    dialog.hide();
+                }
+            });
+
+            dialog.show();
+
+        });
+	},
+});
